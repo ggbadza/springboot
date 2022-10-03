6e7ced5824f342ce54781850d0c89321351fce97
milu <ggbadza@gmail.com>
2022-10-03 15:06:44 +0900

    init

---
+++ src/main/java/com/tankmilu/milu/MiluApplication.java
@@ -0,0 +1,13 @@
+package com.tankmilu.milu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tankmilu/milu/MiluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tankmilu.milu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
